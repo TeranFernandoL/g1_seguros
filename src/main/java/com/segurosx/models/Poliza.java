94c973ab351d9fcfa452b4043580acee8277ce96
Fernando <fernand.teran96@gmail.com>
2020-06-25 06:45:04 -0500

    first commit

---
+++ src/main/java/com/segurosx/models/Poliza.java
@@ -0,0 +1,14 @@
+package com.segurosx.models;
+
+public class Poliza {
+
+    private Integer numero;
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+}
---
+++ src/main/java/com/segurosx/models/SeguroVehicular.java
@@ -0,0 +1,29 @@
+package com.segurosx.models;
+
+public class SeguroVehicular extends Seguro {
+
+    
+    public SeguroVehicular(String marca, String modelo)    {
+
+        super();
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    @Override
+    public void cacularRiesgo()   {
+
+        if (this.marca.equals("Toyota") && this.modelo.equals("Yaris")) {
+            this.nivelRiesgo = "ALTO";
+        }
+        else {
+            this.nivelRiesgo = "BAJO";
+        } 
+    }
+
+    @Override
+    public String getDetalleSeguro()    {
+
+        return "Seg. Vehicular Numero: " + this.numero + " con riesgo: " + this.nivelRiesgo;
+    }    
+}
